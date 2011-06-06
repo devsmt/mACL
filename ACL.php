b644b8c2c2b28f2022ea03f158b0575edbc1fff5
taz <devsmt@gmail.com>
2011-06-06 23:01:41 +0200

    updated tests

--- ACL.php
+++ ACL.php
@@ -1,16 +1,16 @@
 <?php
 /*
 porpuse:
-    determine if a user can access a resource and eventually do an action on the resource
+    determine if a user can access a resource and eventually can do an action on the resource
 */
 class ACL {
     
-    function is_allowed( ArrayAccess $ACL, $user_id, array $roles, $resource_id, $action = '*'  ){
-        $allowed = ACL::user_is_allowed($ACL, $user_id, $resource_id, $action );
+    public static function isAllowed( ArrayAccess $ACL, $user_id, array $roles, $resource_id, $action = '*'  ){
+        $allowed = ACL::userIsAllowed($ACL, $user_id, $resource_id, $action );
         
         if( !$allowed ){
             foreach( $roles as $role ){
-                $allowed = ACL::role_is_allowed($ACL, $role, $resource_id, $action );
+                $allowed = ACL::roleIsAllowed($ACL, $role, $resource_id, $action );
                 if( $allowed ){
                     break;
                 }
@@ -24,20 +24,20 @@ class ACL {
     i dati contenuti nella lista $ACL
     $resource_id può essere qualunque cosa, controllers, tabelle, files
     */
-    public static function user_is_allowed(ArrayAccess $ACL, $user_id, $resource_id, $action = '*' ){
+    public static function userIsAllowed(ArrayAccess $ACL, $user_id, $resource_id, $action = '*' ){
         $allowed = false;
 
         
         if(  $user_id === '__groups__' ){
-            die("non puoi chiamre ACL::user_is_allowed di un utente con id '__groups__'! "); 
+            die("non puoi chiamre ACL::userIsAllowed di un utente con id '__groups__'! "); 
         }
         
         if( !is_string($resource_id) || empty($resource_id) ){
-            die("non puoi chiamre ACL::user_is_allowed per una risorsa vuota:".var_dump($resource_id) ); 
+            die("non puoi chiamre ACL::userIsAllowed per una risorsa vuota:".var_dump($resource_id) ); 
         }
         
         if( !is_string($user_id) || empty($user_id) ){
-            die("non puoi chiamre ACL::user_is_allowed per uno user vuoto:".var_dump($resource_id) ); 
+            die("non puoi chiamre ACL::userIsAllowed per uno user vuoto:".var_dump($resource_id) ); 
         }        
         
         // se non è chiaro a cosa si sta accedendo, si sta richiedendo accesso con pieni poteri
@@ -80,13 +80,14 @@ class ACL {
     /*
     Role based access list
     */
-    function role_is_allowed(ArrayAccess $ACL, $role, $resource_id, $action = '*' ){
+    public static function roleIsAllowed(ArrayAccess $ACL, $role, $resource_id, $action = '*' ){
         $allowed = false;
         
-        // se non è chiaro a cosa si sta accedendo, si sta richiedendo accesso con pieni poteri
+        // if is not clear what the action will be, assume "all" actions
         if( !is_string($action) ){
             $action = '*';
-        }   
+        }
+        // if acl doesnt know obout groups...
         if( !isset($ACL['__groups__']) ){
             return false;
         }
@@ -95,14 +96,14 @@ class ACL {
         
         if( isset($GACL[$role]) ){
             
-            // è superadmin, accede a qualsiasi risorsa
+            // superadmin, can access all resources
             if( $GACL[$role] === '*' || in_array('*', $GACL[$role]) ){
                 
                 $allowed = true;
             } elseif( isset($GACL[$role][$resource_id]) ){
                 
-                // se è admin, ha tutti i privilegi sulla risorsa 
-                // o ha il privilegio specifico sulla srisorsa
+                // if is admin, and than has all privileges over the resource
+                // or har the specific right
                 if( $GACL[$role][$resource_id] == '*' 
                     || in_array( '*', $GACL[$role][$resource_id] ) 
                     || in_array( $action, $GACL[$role][$resource_id] )  
@@ -123,15 +124,13 @@ class ACL {
 }
 
 
-
-
-
-
 /*
-    il più semplice tipo di contenitore di dato, un decoratore di array
+   the simplest, but general enought, data storage possible
 */
 class ACL_starage_array implements ArrayAccess {
+    
     private $data = array();
+    
     public function __construct($data) {
         $this->data = $data;
     }
@@ -143,7 +142,7 @@ class ACL_starage_array implements ArrayAccess {
         return isset($this->data[$offset]) ? $this->data[$offset] : null;
     }
     
-    // metodi per la scrittura, non necessari in questo caso
+    // write methods, not needed for the moment
     public function offsetSet($offset, $value) {
     //    if (is_null($offset)) {
     //        $this->data[] = $value;
@@ -156,94 +155,3 @@ class ACL_starage_array implements ArrayAccess {
     }    
 }
 
-
-//------------------------------------------------------------------------------
-//  minimalist testing
-//------------------------------------------------------------------------------
-
-function ok($test, $msg){
-    if( $test !== true ){
-        echo "! $msg <br>\n";
-    }
-}
-
-// the framework should configure and load data
-
-$ACL = new ACL_starage_array(
-    array(
-    'joe' => array(
-            'res' => array('delete'),
-            'res_all' => array('*')
-        ),
-    'root' => array( '*' ),
-    'toor' => '*',
-    
-    '__groups__' => array(
-            'admin' => array(
-                'res' => array('*')
-            ),
-            'admin2' => array(
-                '*'
-            ),  
-            'admin3' =>  '*',
-            
-            'writer' => array(
-                'res' =>array('add')
-            ),
-            'editor' => array(
-                'res' =>array('add', 'edit')
-            ),
-            'supervisor' => array(
-                'res' =>array('add', 'edit', 'delete')
-            )
-        )
-) );
-
-
-//------------------------------------------------------------------------------
-//  tests
-//------------------------------------------------------------------------------
-
-
-ok( ACL::user_is_allowed($ACL, 'joe', 'res_all'), 'joe.res_all should be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res_all', '*'), 'joe.res_all should be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res_all', null), 'joe.res_all should be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res_all', -1), 'joe.res_all should be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res_all', false), 'joe.res_all should be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res_all', 1), 'joe.res_all should be allowed' );
-
-
-ok( ACL::user_is_allowed($ACL, 'joe', 'res', 'delete'), 'joe.res.delete should be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res', 'create')===false, 'joe.res.create should not be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res') === false, 'joe.res.* should not be allowed' );
-// junk values translate to '*' request permission, which is false
-ok( ACL::user_is_allowed($ACL, 'joe', 'res', null) === false, 'joe.res.* should not be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res', false) === false, 'joe.res.* should not be allowed' );
-ok( ACL::user_is_allowed($ACL, 'joe', 'res', -1) === false, 'joe.res.* should not be allowed' );
-
-
-ok( ACL::user_is_allowed($ACL, 'non_existing_user', 'res') === false, 'non_existing_user.* should not be allowed to anything' );
-
-
-ok( ACL::user_is_allowed($ACL, 'joe', 'res_not_exixsting') === false, 'joe.res_not_exixsting.* should not be allowed' );
-
-
-ok( ACL::user_is_allowed($ACL, 'root', 'res_all'), 'root.res_all should be allowed' );
-                                                
-ok( ACL::user_is_allowed($ACL, 'toor', 'res_all'), 'toor.res_all should be allowed' );
-
-
-
-ok( ACL::role_is_allowed($ACL, 'admin', 'res'), 'role admin.res should be allowed' );
-ok( ACL::role_is_allowed($ACL, 'admin', 'res', 'add'), 'role admin.res.add should be allowed' ); 
- 
-
-ok( ACL::role_is_allowed($ACL, 'admin2', 'res'), 'role admin2.res should be allowed' );
-ok( ACL::role_is_allowed($ACL, 'admin3', 'res'), 'role admin3.res should be allowed' );
-
-ok( ACL::role_is_allowed($ACL, 'writer', 'edit') === false, 'role writer.edit should not be allowed' );
-
-
-ok( ACL::is_allowed( $ACL, 'joe', array(), 'res', 'add'  ) === false, 'joe should not access to res.add' );
-ok( ACL::is_allowed( $ACL, 'joe', array('admin'), 'res', 'add'  ), 'joe cant access to res.add, but his group "admin" should' );
-  
--- ACLTest.php
+++ ACLTest.php
@@ -1,92 +1,98 @@
 <?php
 
-//------------------------------------------------------------------------------
-//  minimalist testing
-//------------------------------------------------------------------------------
+require_once 'ACL.php';
 
-function ok($test, $msg){
-    if( $test !== true ){
-        echo "! $msg <br>\n";
+class ACLTest extends PHPUnit_Framework_TestCase
+{
+    
+    
+    
+    
+    public function testACL()
+    {
+        // the framework should configure and load data
+        
+        $ACL = new ACL_starage_array(
+            array(
+            'joe' => array(
+                    'res' => array('delete'),
+                    'res_all' => array('*')
+                ),
+            'root' => array( '*' ),
+            'toor' => '*',
+            
+            '__groups__' => array(
+                    'admin' => array(
+                        'res' => array('*')
+                    ),
+                    'admin2' => array(
+                        '*'
+                    ),  
+                    'admin3' =>  '*',
+                    
+                    'writer' => array(
+                        'res' =>array('add')
+                    ),
+                    'editor' => array(
+                        'res' =>array('add', 'edit')
+                    ),
+                    'supervisor' => array(
+                        'res' =>array('add', 'edit', 'delete')
+                    )
+                )
+        ) );
+
+        //------------------------------------------------------------------------------
+        //  tests
+        //------------------------------------------------------------------------------
+        
+        
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'joe', 'res_all'), 'joe.res_all should be allowed' );
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'joe', 'res_all', '*'), 'joe.res_all should be allowed' );
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'joe', 'res_all', null), 'joe.res_all should be allowed' );
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'joe', 'res_all', -1), 'joe.res_all should be allowed' );
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'joe', 'res_all', false), 'joe.res_all should be allowed' );
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'joe', 'res_all', 1), 'joe.res_all should be allowed' );
+        
+        
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'joe', 'res', 'delete'), 'joe.res.delete should be allowed' );
+        $this->assertEquals(false, ACL::userIsAllowed($ACL, 'joe', 'res', 'create'), 'joe.res.create should not be allowed' );
+        $this->assertEquals(false, ACL::userIsAllowed($ACL, 'joe', 'res'), 'joe.res.* should not be allowed' );
+        // junk values translate to '*' request permission, which is false
+        $this->assertEquals(false, ACL::userIsAllowed($ACL, 'joe', 'res', null), 'joe.res.* should not be allowed' );
+        $this->assertEquals(false, ACL::userIsAllowed($ACL, 'joe', 'res', false), 'joe.res.* should not be allowed' );
+        $this->assertEquals(false, ACL::userIsAllowed($ACL, 'joe', 'res', -1), 'joe.res.* should not be allowed' );
+        
+        
+        $this->assertEquals(false, ACL::userIsAllowed($ACL, 'non_existing_user', 'res'), 'non_existing_user.* should not be allowed to anything' );
+        
+        
+        $this->assertEquals(false, ACL::userIsAllowed($ACL, 'joe', 'res_not_exixsting'), 'joe.res_not_exixsting.* should not be allowed' );
+        
+        
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'root', 'res_all'), 'root.res_all should be allowed' );
+                                                        
+        $this->assertEquals(true, ACL::userIsAllowed($ACL, 'toor', 'res_all'), 'toor.res_all should be allowed' );
+        
+        
+        
+        $this->assertEquals(true, ACL::roleIsAllowed($ACL, 'admin', 'res'), 'role admin.res should be allowed' );
+        $this->assertEquals(true, ACL::roleIsAllowed($ACL, 'admin', 'res', 'add'), 'role admin.res.add should be allowed' ); 
+         
+        
+        $this->assertEquals(true, ACL::roleIsAllowed($ACL, 'admin2', 'res'), 'role admin2.res should be allowed' );
+        $this->assertEquals(true, ACL::roleIsAllowed($ACL, 'admin3', 'res'), 'role admin3.res should be allowed' );
+        
+        $this->assertEquals(false, ACL::roleIsAllowed($ACL, 'writer', 'edit'), 'role writer.edit should not be allowed' );
+        
+        
+        $this->assertEquals(false, ACL::isAllowed( $ACL, 'joe', array(), 'res', 'add'  ), 'joe should not access to res.add' );
+        $this->assertEquals(true, ACL::isAllowed( $ACL, 'joe', array('admin'), 'res', 'add'  ), 'joe cant access to res.add, but his group "admin" should' );
+  
     }
 }
-
-// the framework should configure and load data
-
-$ACL = new ACL_starage_array(
-    array(
-    'joe' => array(
-            'res' => array('delete'),
-            'res_all' => array('*')
-        ),
-    'root' => array( '*' ),
-    'toor' => '*',
-    
-    '__groups__' => array(
-            'admin' => array(
-                'res' => array('*')
-            ),
-            'admin2' => array(
-                '*'
-            ),  
-            'admin3' =>  '*',
-            
-            'writer' => array(
-                'res' =>array('add')
-            ),
-            'editor' => array(
-                'res' =>array('add', 'edit')
-            ),
-            'supervisor' => array(
-                'res' =>array('add', 'edit', 'delete')
-            )
-        )
-) );
-
-
-//------------------------------------------------------------------------------
-//  tests
-//------------------------------------------------------------------------------
-
-
-ok( ACL::userIsAllowed($ACL, 'joe', 'res_all'), 'joe.res_all should be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res_all', '*'), 'joe.res_all should be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res_all', null), 'joe.res_all should be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res_all', -1), 'joe.res_all should be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res_all', false), 'joe.res_all should be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res_all', 1), 'joe.res_all should be allowed' );
-
-
-ok( ACL::userIsAllowed($ACL, 'joe', 'res', 'delete'), 'joe.res.delete should be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res', 'create')===false, 'joe.res.create should not be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res') === false, 'joe.res.* should not be allowed' );
-// junk values translate to '*' request permission, which is false
-ok( ACL::userIsAllowed($ACL, 'joe', 'res', null) === false, 'joe.res.* should not be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res', false) === false, 'joe.res.* should not be allowed' );
-ok( ACL::userIsAllowed($ACL, 'joe', 'res', -1) === false, 'joe.res.* should not be allowed' );
-
-
-ok( ACL::userIsAllowed($ACL, 'non_existing_user', 'res') === false, 'non_existing_user.* should not be allowed to anything' );
-
-
-ok( ACL::userIsAllowed($ACL, 'joe', 'res_not_exixsting') === false, 'joe.res_not_exixsting.* should not be allowed' );
-
-
-ok( ACL::userIsAllowed($ACL, 'root', 'res_all'), 'root.res_all should be allowed' );
-                                                
-ok( ACL::userIsAllowed($ACL, 'toor', 'res_all'), 'toor.res_all should be allowed' );
-
-
-
-ok( ACL::roleIsAllowed($ACL, 'admin', 'res'), 'role admin.res should be allowed' );
-ok( ACL::roleIsAllowed($ACL, 'admin', 'res', 'add'), 'role admin.res.add should be allowed' ); 
  
 
-ok( ACL::roleIsAllowed($ACL, 'admin2', 'res'), 'role admin2.res should be allowed' );
-ok( ACL::roleIsAllowed($ACL, 'admin3', 'res'), 'role admin3.res should be allowed' );
-
-ok( ACL::roleIsAllowed($ACL, 'writer', 'edit') === false, 'role writer.edit should not be allowed' );
 
 
-ok( ACL::isAllowed( $ACL, 'joe', array(), 'res', 'add'  ) === false, 'joe should not access to res.add' );
-ok( ACL::isAllowed( $ACL, 'joe', array('admin'), 'res', 'add'  ), 'joe cant access to res.add, but his group "admin" should' );
-  
+
