e11c773b2daea779e5ca1700d233a2d297c25855
taz <devsmt@gmail.com>
2011-06-06 23:14:42 +0200

    format

--- ACL.php
+++ ACL.php
@@ -4,154 +4,115 @@ porpuse:
     determine if a user can access a resource and eventually can do an action on the resource
 */
 class ACL {
-    
-    public static function isAllowed( ArrayAccess $ACL, $user_id, array $roles, $resource_id, $action = '*'  ){
-        $allowed = ACL::userIsAllowed($ACL, $user_id, $resource_id, $action );
-        
-        if( !$allowed ){
-            foreach( $roles as $role ){
-                $allowed = ACL::roleIsAllowed($ACL, $role, $resource_id, $action );
-                if( $allowed ){
+    public static function isAllowed(ArrayAccess $ACL, $user_id, array $roles, $resource_id, $action = '*') {
+        $allowed = ACL::userIsAllowed($ACL, $user_id, $resource_id, $action);
+        if (!$allowed) {
+            foreach ($roles as $role) {
+                $allowed = ACL::roleIsAllowed($ACL, $role, $resource_id, $action);
+                if ($allowed) {
                     break;
                 }
             }
         }
         return $allowed;
     }
-    
     /*
     determina se, l'utente selezionato ha permessi per la risorsa+azione secondo 
     i dati contenuti nella lista $ACL
     $resource_id può essere qualunque cosa, controllers, tabelle, files
     */
-    public static function userIsAllowed(ArrayAccess $ACL, $user_id, $resource_id, $action = '*' ){
+    public static function userIsAllowed(ArrayAccess $ACL, $user_id, $resource_id, $action = '*') {
         $allowed = false;
-
-        
-        if(  $user_id === '__groups__' ){
-            die("non puoi chiamre ACL::userIsAllowed di un utente con id '__groups__'! "); 
+        if ($user_id === '__groups__') {
+            die("non puoi chiamre ACL::userIsAllowed di un utente con id '__groups__'! ");
         }
-        
-        if( !is_string($resource_id) || empty($resource_id) ){
-            die("non puoi chiamre ACL::userIsAllowed per una risorsa vuota:".var_dump($resource_id) ); 
+        if (!is_string($resource_id) || empty($resource_id)) {
+            die("non puoi chiamre ACL::userIsAllowed per una risorsa vuota:" . var_dump($resource_id));
         }
-        
-        if( !is_string($user_id) || empty($user_id) ){
-            die("non puoi chiamre ACL::userIsAllowed per uno user vuoto:".var_dump($resource_id) ); 
-        }        
-        
+        if (!is_string($user_id) || empty($user_id)) {
+            die("non puoi chiamre ACL::userIsAllowed per uno user vuoto:" . var_dump($resource_id));
+        }
         // se non è chiaro a cosa si sta accedendo, si sta richiedendo accesso con pieni poteri
-        if( !is_string($action) ){
+        if (!is_string($action)) {
             $action = '*';
-        } 
-
-        
-        if( isset($ACL[$user_id]) ){
-            
-            if( $ACL[$user_id] === '*' || in_array('*', $ACL[$user_id]) ){
+        }
+        if (isset($ACL[$user_id])) {
+            if ($ACL[$user_id] === '*' || in_array('*', $ACL[$user_id])) {
                 // è superadmin, accede a qualsiasi risorsa
                 $allowed = true;
-            } elseif( isset($ACL[$user_id][$resource_id]) ){
-                
+            } elseif (isset($ACL[$user_id][$resource_id])) {
                 // nessuna particolare azione, l'utente è abilitato di default
                 // l'utente ha tutti i permessi su tutta la risorsa, l'utente è abilitato
-                if( isset($ACL[$user_id][$resource_id]['*']) ){
+                if (isset($ACL[$user_id][$resource_id]['*'])) {
                     $allowed = true;
                 } else {
-                    if( in_array( $action, $ACL[$user_id][$resource_id] )  ){
+                    if (in_array($action, $ACL[$user_id][$resource_id])) {
                         $allowed = true;
                     } else {
-                        //  debug
                         $allowed = false;
                     }
                 }
-            } else {
-                // debug
-                //echo "'$action' not allowed for \$ACL[$user_id][$resource_id], which is: ".implode(',', $ACL[$user_id][$resource_id]);
             }
-        } else {
-            // debug
-            //echo "user not found !isset(\$ACL[$user_id])";
         }
-        
         return $allowed;
-    } 
-    
+    }
     /*
     Role based access list
     */
-    public static function roleIsAllowed(ArrayAccess $ACL, $role, $resource_id, $action = '*' ){
+    public static function roleIsAllowed(ArrayAccess $ACL, $role, $resource_id, $action = '*') {
         $allowed = false;
-        
         // if is not clear what the action will be, assume "all" actions
-        if( !is_string($action) ){
+        if (!is_string($action)) {
             $action = '*';
         }
         // if acl doesnt know obout groups...
-        if( !isset($ACL['__groups__']) ){
+        if (!isset($ACL['__groups__'])) {
             return false;
         }
-        
         $GACL = $ACL['__groups__'];
-        
-        if( isset($GACL[$role]) ){
-            
+        if (isset($GACL[$role])) {
             // superadmin, can access all resources
-            if( $GACL[$role] === '*' || in_array('*', $GACL[$role]) ){
-                
+            if ($GACL[$role] === '*' || in_array('*', $GACL[$role])) {
                 $allowed = true;
-            } elseif( isset($GACL[$role][$resource_id]) ){
-                
+            } elseif (isset($GACL[$role][$resource_id])) {
                 // if is admin, and than has all privileges over the resource
                 // or har the specific right
-                if( $GACL[$role][$resource_id] == '*' 
-                    || in_array( '*', $GACL[$role][$resource_id] ) 
-                    || in_array( $action, $GACL[$role][$resource_id] )  
-                   ){
-                
+                if ($GACL[$role][$resource_id] == '*' || in_array('*', $GACL[$role][$resource_id]) || in_array($action, $GACL[$role][$resource_id])) {
                     $allowed = true;
                 } else {
-                    //  debug
                     $allowed = false;
-                }                
+                }
             }
-        } else {
-        }
+        } 
         
         return $allowed;
-    }     
-    
+    }
 }
-
-
 /*
    the simplest, but general enought, data storage possible
 */
 class ACL_starage_array implements ArrayAccess {
-    
     private $data = array();
-    
     public function __construct($data) {
         $this->data = $data;
     }
     public function offsetExists($offset) {
         return isset($this->data[$offset]);
     }
-
     public function offsetGet($offset) {
         return isset($this->data[$offset]) ? $this->data[$offset] : null;
     }
-    
     // write methods, not needed for the moment
     public function offsetSet($offset, $value) {
-    //    if (is_null($offset)) {
-    //        $this->data[] = $value;
-    //    } else {
-    //        $this->data[$offset] = $value;
-    //    }
+        //    if (is_null($offset)) {
+        //        $this->data[] = $value;
+        //    } else {
+        //        $this->data[$offset] = $value;
+        //    }
+        
     }
     public function offsetUnset($offset) {
-    //    unset($this->data[$offset]);
-    }    
+        //    unset($this->data[$offset]);
+        
+    }
 }
-
